77a2604353a6e42de7742633848726230395491b
Skylarx23 <pepsiix23@gmail.com>
2023-04-04 18:35:19 +0100

    Initializing
    Initialized project as of 31/03/2023

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    public float speed = 12f;
+    public Vector3 velocity;
+    public float gravity = -9.81f;
+
+    [SerializeField] Transform groundCheck;
+    public float groundRadius = 0.4f;
+    public LayerMask groundLayer;
+
+    bool isGrounded;
+    public float jumpHeight = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Creates a small sphere to check if the player is touching anything with "ground" layer
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundLayer);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Sets the varibles to the Key's direction 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Moves the player (Relative to Itself) using the previous varibles 
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Applys Gravity To The Player
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+    }
+}
