ba45a5bd2fac10aab23423689c205ea1576ae494
Skylarx23 <pepsiix23@gmail.com>
2023-05-07 17:16:39 +0100

    Pistols / animations
    Added the pistol model to the game
    
    Also added 2 animations for the rifle and 3 animations for the pistol

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,10 +8,17 @@ public class GameManager : MonoBehaviour
     public Slider HealthBar;
     public GameObject enemy;
 
+    public AudioSource soundSource;
+    public AudioClip queenFightMusic;
+    public AudioClip idleMusic1;
+    public AudioClip fightTheme;
+    public AudioClip ambience1, ambience2;
+
     // Start is called before the first frame update
     void Start()
     {
         //SpawnScript.SpawnEnemies(4, Enemy);
+        soundSource.PlayOneShot(idleMusic1, 0.3f);
     }
 
     // Update is called once per frame
